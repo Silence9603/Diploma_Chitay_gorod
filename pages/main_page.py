b5d47822c2a40c4019b2cba5aa15ab062016a377
Silence <zamani1908@mail.ru>
2026-05-14 16:12:02 +0700

    Начало работы2

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,11 +1,15 @@
-from selenium.webdriver.common.by import By
 from .base_page import BasePage
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import allure
 
 
 class MainPage(BasePage):
     # Локаторы
-    SEARCH_INPUT = (By.CSS_SELECTOR, "input[type='search'], .search-input, input[placeholder*='Поиск']")
+    #SEARCH_INPUT = (By.CSS_SELECTOR, "input[type='search'], .search-input, input[placeholder*='Поиск']")
     SEARCH_BUTTON = (By.CSS_SELECTOR, "button[aria-label='Поиск'], .search-button")
     CART_ICON = (By.CSS_SELECTOR, ".cart-icon, [data-testid='cart']")
     CART_COUNTER = (By.CSS_SELECTOR, ".cart-counter, .cart-count")
@@ -14,6 +18,13 @@ class MainPage(BasePage):
     FIRST_BOOK = (By.CSS_SELECTOR, ".product-card:first-child, .book-item:first-child")
     PAGE_TITLE = (By.TAG_NAME, "h1")
     BANNER_CAROUSEL = (By.CSS_SELECTOR, ".banner-carousel, .main-slider")
+    SEARCH_INPUT = (By.XPATH, '//input[@id="app-search"]')
+    TEXT_SEARCH_RESULT = (By.XPATH, "//h1[@class='search-title__head' and contains(text(), 'Результаты поиска')]")
+    BOOK_TITLE = (By.XPATH, '//a[@class="product-card__title"]')
+
+    #Адреса
+
+    URL = "https://www.chitai-gorod.ru/"
 
     def __init__(self, driver):
         super().__init__(driver)
@@ -46,3 +57,22 @@ class MainPage(BasePage):
         if self.is_element_visible(self.CART_COUNTER, timeout=2):
             return int(self.get_text(self.CART_COUNTER))
         return 0
+
+    def open_main_page(self)-> None:
+        '''Открывает главную страницу'''
+        self.driver.get(self.URL)
+
+    def enter_title(self, title: str) -> None:
+        '''Печатает название книги в строку поиска'''
+        input = self.wait.until(EC.presence_of_element_located(self.SEARCH_INPUT))
+        input.clear()
+        input.send_keys(title)
+        input.send_keys(Keys.RETURN)
+
+    def is_search_result_page(self):
+        '''Проверяет, открылась ли страница с результатами поиска'''
+        assert self.wait.until(EC.presence_of_element_located(self.TEXT_SEARCH_RESULT))
+
+    def get_search_result(self):
+        '''Возвращает список найденных книг'''
+        books = self.find_element(self.BOOK_TITLE)
--- tests/UI_test.py
+++ tests/UI_test.py
@@ -1,8 +1,10 @@
-from pages.UI_page import MainPage
+from pages.main_page import MainPage
 
 
-def test1(driver):
+def test_search_book(driver):
+    book = 'Маленький принц'
     main = MainPage(driver)
 
 #поиск по ключевому слову (Маленький принц) id="app-search"
-    main.open()
+    main.open_main_page()
+    main.enter_title(book)
